2ea7b5bc6c4c4d5c58d6abbd9abe54744ce52da2
Hiroyuki Yamaoka <yamaoka@kotori.in>
2011-09-14 00:33:35 +0900

    add Application::code

--- Karinto.php
+++ Karinto.php
@@ -33,7 +33,7 @@ class Application
     {
         // send headers
         foreach ($this->_headers as $name => $value) {
-            if (ctype_digit(strval($name)) {
+            if (ctype_digit(strval($name))) {
                 header($value);
             } else {
                 header("{$name}: {$value}");
@@ -81,7 +81,7 @@ class Application
         }
     }
 
-    public function print($text)
+    public function output($text)
     {
         $this->_body .= $text;
     }
@@ -111,14 +111,14 @@ class Application
         } catch (Exception $e) {
             $this->code(404);
         }
-        $this->print($result);
+        $this->output($result);
     }
 
     public function json($values)
     {
         $json = json_encode($values);
         $this->contentType('application/json');
-        $this->print($json);
+        $this->output($json);
     }
 
     public function redirect($url, $code = 302, $isSecure = false)
@@ -132,7 +132,7 @@ class Application
         }
         $this->code($code);
         $this->header('Location', $url);
-        $this->print('<html><head><meta http-equiv="refresh" content="0;'
+        $this->output('<html><head><meta http-equiv="refresh" content="0;'
             . 'url=' . htmlentities($url, ENT_QUOTES) . '"></head></html>');
     }
 
@@ -160,6 +160,68 @@ class Application
 
     public function code($code)
     {
+        $messages = array(
+            // Informational 1xx
+            100 => 'Continue',
+            101 => 'Switching Protocols',
+            // Success 2xx
+            200 => 'OK',
+            201 => 'Created',
+            202 => 'Accepted',
+            203 => 'Non-Authoritative Information',
+            204 => 'No Content',
+            205 => 'Reset Content',
+            206 => 'Partial Content',
+            // Redirection 3xx
+            300 => 'Multiple Choices',
+            301 => 'Moved Permanently',
+            302 => 'Found',  // 1.1
+            303 => 'See Other',
+            304 => 'Not Modified',
+            305 => 'Use Proxy',
+            // 306 is no longer used but still reserved
+            307 => 'Temporary Redirect',
+            // Client Error 4xx
+            400 => 'Bad Request',
+            401 => 'Unauthorized',
+            402 => 'Payment Required',
+            403 => 'Forbidden',
+            404 => 'Not Found',
+            405 => 'Method Not Allowed',
+            406 => 'Not Acceptable',
+            407 => 'Proxy Authentication Required',
+            408 => 'Request Timeout',
+            409 => 'Conflict',
+            410 => 'Gone',
+            411 => 'Length Required',
+            412 => 'Precondition Failed',
+            413 => 'Request Entity Too Large',
+            414 => 'Request-URI Too Long',
+            415 => 'Unsupported Media Type',
+            416 => 'Requested Range Not Satisfiable',
+            417 => 'Expectation Failed',
+            // Server Error 5xx
+            500 => 'Internal Server Error',
+            501 => 'Not Implemented',
+            502 => 'Bad Gateway',
+            503 => 'Service Unavailable',
+            504 => 'Gateway Timeout',
+            505 => 'HTTP Version Not Supported',
+            509 => 'Bandwidth Limit Exceeded',
+        );
+        if (isset($messages[$code])) {
+            if ($this->httpVersion !== '1.1') {
+                // HTTP/1.0
+                $messages[302] = 'Moved Temporarily';
+            }
+            $message = $messages[$code];
+            $this->header('Status', "{$code} {$message}");
+            $this->header(null,
+                "HTTP/{$this->httpVersion} {$code} {$message}");
+        }
+        if ($code >= 400) {
+            // error
+        }
     }
 
     public function cookie($name, $value, $expire = null,
