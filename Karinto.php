644b97a2a558148016f4b624d2304a2bc45fba2e
Hiroyuki Yamaoka <yamaoka@kotori.in>
2011-09-14 14:38:59 +0900

    implement Application::session

--- Karinto.php
+++ Karinto.php
@@ -6,6 +6,7 @@ class Application
     public $templateDir = 'templates';
     public $encoding = 'UTF-8';
     public $httpVersion = '1.1';
+    public $sessionSecretKey = null;
 
     protected $_routes = array(
         'GET' => array(), 'POST' => array(),
@@ -236,6 +237,11 @@ class Application
 
     public function session()
     {
+        if (strlen($this->sessionSecretKey) === 0) {
+            throw new Exception('session secret key is not set');
+        }
+        $session = new Session($this->sessionSecretKey);
+        return $session;
     }
 
     public function run()
@@ -332,6 +338,124 @@ class Request
 
 class Session
 {
+    const COOKIE_MAX_LENGTH = 4096;
+
+    protected $_secretKey;
+    protected $_vars = array();
+    protected $_isAvailable = false;
+    protected $_cookieName;
+    protected $_cookieParams;
+
+    public function __construct($secretKey)
+    {
+        $this->_secretKey = $secretKey;
+        $this->_isAvailable = true;
+
+        // use session settings
+        $this->_cookieName = session_name();
+        $this->_cookieParams = session_get_cookie_params();
+        $this->_restore();
+    }
+
+    public function __set($name, $value)
+    {
+        $this->_vars[$name] = $value;
+    }
+
+    public function __get($name)
+    {
+        if (isset($this->_vars[$name])) {
+            return $this->_vars[$name];
+        }
+        return null;
+    }
+
+    public function __isset($name)
+    {
+        return isset($this->_vars[$name]);
+    }
+
+    public function __unset($name)
+    {
+        if (isset($this->_vars[$name])) {
+            unset($this->_vars[$name]);
+        }
+    }
+
+    public function save()
+    {
+        if (!$this->_isAvailable) {
+            return;
+        }
+        $expire = time() + $this->cookieParam['lifetime'];
+        $cookieData = base64_encode(serialize($this->_vars))
+            . '--' . $this->_digest($this->_vars);
+        if (strlen($cookieData) > self::COOKIE_MAX_LENGTH) {
+            throw new Exception('The session data is too large.');
+        }
+        $this->_sendCookie($cookieData, $expire);
+    }
+
+    public function setLifetime($seconds)
+    {
+        $this->_cookieParams['lifetime'] = $seconds;
+    }
+
+    public function destroy()
+    {
+        $this->_isAvailable = false;
+        $this->_vars = array();
+        $this->_sendCookie('', time() - 3600);
+    }
+
+    protected function _restore()
+    {
+        if (!isset($_COOKIE[$this->cookieName])) {
+            // cookie not exists
+            return;
+        }
+        $cookieData = $_COOKIE[$this->_cookieName];
+        $dataList = explode('--', $cookieData);
+        if (count($dataList) !== 2) {
+            // invalid cookie value
+            $this->destroy();
+            return;
+        }
+        $vars = unserialize(base64_decode($dataList[0]));
+        if ($vars === false || !is_array($vars)) {
+            // broken cookie value
+            $this->destroy();
+            return;
+        }
+        if ($this->_digest($vars) !== $dataList[1]) {
+            // tampered
+            $this->destroy();
+            return;
+        }
+        $this->_vars = $vars;
+    }
+
+    protected function _digest($data)
+    {
+        $serializedData = serialize($data);
+        return hash_hmac('sha1', $serializedData, $this->_secretKey);
+    }
+
+    protected function _sendCookie($value, $expire)
+    {
+        $name = $this->_cookieName;
+        $params = $this->_cookieParams;
+
+        if (empty($params['domain']) && empty ($params['secure'])) {
+            setcookie($name, $value, $expire, $params['path']);
+        } elseif (empty($params['secure'])) {
+            setcookie($name, $value, $expire,
+                $params['path'], $params['domain']);
+        } else {
+            setcookie($name, $value, $expire,
+                $params['path'], $params['domain'], $params['secure']);
+        }
+    }
 }
 
 class Utils
